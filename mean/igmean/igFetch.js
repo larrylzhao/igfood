20fc0a5424032b9c246f40d9b20ff1e2c3512efb
lingyuan zhao <larryzhao@lingyuans-Pro.attlocal.net>
2016-04-02 20:49:14 -0500

    api hello world

--- mean/igmean/igFetch.js
+++ mean/igmean/igFetch.js
@@ -1,5 +1,30 @@
 var ig = require('instagram-node').instagram();
+var request = require('request');
 
 var config = require('../../lztest/config.json');
 
-console.log(config);
+//console.log(config);
+config.access_token = "16384709.6ac06b4.49b97800d7fd4ac799a2c889f50f2587";
+ig.use({ access_token: "config.access_token",
+			//client_id: config.client_id,
+         	//client_secret: config.client_secret 
+});
+
+var tagName = 'coding';
+var count = '2';
+var queryString = 'https://api.instagram.com/v1/tags/' + tagName + '/media/recent?access_token='
++ config.access_token + '&count=' + count;
+
+console.log(queryString);
+
+request(queryString, function (err, res, body) {
+	//console.log(err);
+	//console.log(res);
+	//console.log(body);
+	var reqBody = body.toString();
+	reqBody = JSON.parse(reqBody);
+	//console.log(reqBody);
+	console.log(reqBody.data)
+});
+
+
