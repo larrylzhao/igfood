331b8dd337c453dea0c7ea2d3277a24e03455c45
n3rdftw <zhou.oliver@gmail.com>
2016-04-03 10:49:41 -0700

    PIL is dead
    PIL is dead, Using Pillow instead

--- processors/Imaging-1.1.7/RGBAValue.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-from PIL import Image
-
-im = Image.open("test.png")
-pix = im.load()
-print im.size #Get the width and hight of the image for iterating over
-print pix[x,y] #Get the RGBA Value of the a pixel of an image
-pix[x,y] = value # Set the RGBA Value of the image (tuple)
