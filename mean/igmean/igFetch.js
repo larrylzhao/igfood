9cf1cff657351b67b436c21183a1be1c6807693a
lingyuan zhao <larryzhao@lingyuans-Pro.attlocal.net>
2016-04-03 21:27:38 -0500

    igFetch.py created for filtering and fetching images

--- mean/igmean/igFetch.js
+++ mean/igmean/igFetch.js
@@ -10,54 +10,69 @@ ig.use({ access_token: "config.access_token",
          	//client_secret: config.client_secret 
 });
 
-var tagName = 'brzetto';
-var count = '5';
-var max_tag_id = "AQBqz7-AE1aB1OumD6_yUH0QgXjLNA6C_Q2IcOHc_Jk4h_4lFzDKnLvPuLO_00LqqPcjIuwhGqHXQQ0N-R2VjbMWqqQns17aFKm4ZhuFjz6WvDsB_TSFqQytsAxR58zMLPzA20iFbn3VSBFDqIn2V0l6trmWLfxgqGSjXkCGmWqkvw";
-var min_tag_id;// = "AQAa6UgrOd579zIOu45H84zuK1bieyUXx1U6Yv9upiwT0akanyZV6hoD4YLHhDc3RjxKuiPFDBGJlXz5YCF6r5yo4fTPQXbmSdVx0LsaQbxKeSTqT5ZhpiHtRqkXtoEfnX9IiIzURiHU2ZOy3BpRBOi8aiOaEvBngr7Elp9HAX-qTA";
-var queryString = 'https://api.instagram.com/v1/tags/' + tagName + '/media/recent?access_token='
-+ config.access_token + '&count=' + count + '&max_tag_id=' + max_tag_id;
+var endTime = '1459048599';
+var startTime = '1459656933';
+var tagName = 'resourcemagazine';
+var count = '10';
+var max_tag_id = '';
 
-console.log(queryString);
-var index = 0;
-
-request(queryString, function (err, res, body) {
-	//console.log(err);
-	//console.log(res);
-	//console.log(body);
-	//var reqBody = body.toString();
-	reqBody = JSON.parse(body);
-
-	/*min_tag_id = reqBody.pagination.min_tag_id;
-	var link = reqBody.data[index].link;
-	var tags = reqBody.data[index].tags.length;
-	var likes = reqBody.data[index].likes.count;
-	var time = reqBody.data[index].created_time;
-	var type = reqBody.data[index].type; //must be 'image'
-	var filter = reqBody.data[index].filter;
-	var thumbnail = reqBody.data[index].images.thumbnail.url;
-	var lowres = reqBody.data[index].images.low_resolution.url;
-	var user = reqBody.data[index].user.username;
-
-	var imageInfo = '\n min_tag_id: ' + min_tag_id +
-					'\n Time: ' + time +
-					'\n type: ' + type +
-					'\n tags: ' + tags +
-					'\n user: ' + user + //need # of subs user has
-					'\n likes: ' + likes +
-					'\n thumbnail: ' + thumbnail +
-					'\n lowres: ' + lowres +
-					'\n';
-	console.log(imageInfo);*/
-	console.log(reqBody.data[4]);
-
-	var fs = require('fs');
-	fs.writeFile("igFetch.txt", body, function(err) {
-	    if(err) {
-	        return console.log(err);
-	    }
-
-    console.log("The file was saved!");
-}); 
-});
+var i = 0;
+//while (startTime > endTime) {
+while (i < 4) {
+	i++;
+	console.log(i + ': ' + max_tag_id);
 
 
+	
+	var queryString = 'https://api.instagram.com/v1/tags/' + tagName + '/media/recent?access_token='
+	+ config.access_token + '&count=' + count + '&max_tag_id=' + max_tag_id;
+
+	//console.log(queryString);
+	var index = 0;
+
+	request(queryString, function (err, res, body) {
+
+		reqBody = JSON.parse(body);
+
+		max_tag_id = reqBody.pagination.next_max_tag_id;
+		var link = reqBody.data[index].link;
+		var tags = reqBody.data[index].tags.length;
+		var likes = reqBody.data[index].likes.count;
+		var time = reqBody.data[index].created_time;
+		var type = reqBody.data[index].type; //must be 'image'
+		var filter = reqBody.data[index].filter;
+		var thumbnail = reqBody.data[index].images.thumbnail.url;
+		var lowres = reqBody.data[index].images.low_resolution.url;
+		var user = reqBody.data[index].user.username;
+
+		var imageInfo = '\n max_tag_id: ' + max_tag_id +
+						'\n Time: ' + time +
+						'\n type: ' + type +
+						'\n tags: ' + tags +
+						'\n user: ' + user + //need # of subs user has
+						'\n likes: ' + likes +
+						'\n thumbnail: ' + thumbnail +
+						'\n lowres: ' + lowres +
+						'\n';
+		console.log(imageInfo);
+		//console.log(reqBody.data[4]);
+
+		startTime = time;
+
+		
+	});
+	var timer = setTimeout(function () {
+        var dataArray = [123, 456, 789, 012, 345, 678];
+        callback(dataArray);
+    }, 3000);
+}
+
+
+/*var fs = require('fs');
+		fs.writeFile("igFetch.txt", body, function(err) {
+		    if(err) {
+		        return console.log(err);
+		    }
+		    console.log("The file was saved!");
+		}); 
+*/
